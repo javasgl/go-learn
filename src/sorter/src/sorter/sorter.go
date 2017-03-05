7c3d52e6e1f25fd800b8d699fb8d4079ba3d2c31
ganglin.song <ganglin.song@yourdream.cc>
2017-03-05 14:53:25 +0800

    feat: sort

---
+++ src/sorter/src/sorter/sorter.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+var infile *string = flag.String("i", "infile", "Input File")
+var outfile *string = flag.String("o", "outfile", "OutInput File")
+var algorithm *string = flag.String("a", "qsort", "Sort Algorithm")
+
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile =", *infile, "outfile = ", *outfile, "algorithm = ", *algorithm)
+	}
+}
+func readValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file ", infile)
+		return
+	}
+	defer file.Close()
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("A too long line!")
+			return
+		}
+		str := string(line)
+		value, err1 := strconv.Atoi(str)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		values = append(values, value)
+	}
+	return
+}
