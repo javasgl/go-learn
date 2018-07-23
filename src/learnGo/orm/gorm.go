02055559dfe4cad9d287265c29b47863fe332ad3
javasgl <javasgl@qq.com>
2018-07-23 16:18:03 +0800

    feat: gorm

---
+++ src/learnGo/orm/gorm.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type TestOrderV2 struct {
+	Id         int64
+	Fee        float64
+	AFee       float64
+	Version    int
+	CreateTime int64
+	UpdateTime int64
+}
+
+func main() {
+
+	db, err := gorm.Open("mysql", "root@/test?charset=utf8")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&TestOrderV2{})
+	// db.Create(&TestOrderV2{Fee: 12.34, AFee: 23.12, Version: 1, CreateTime: time.Now().Unix()})
+	var order TestOrderV2
+	db.First(&order, 1)
+	db.Model(&order).Update("Fee", 2.84)
+	db.Model(&order).Update("AFee", 0)
+	order.Version = 1
+	db.Model(&order).Update()
+}
